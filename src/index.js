260e71d61ea9ab454fdea660ae947e08d8eae6ec
nonaka <electro0701@gmail.com>
2021-08-26 18:01:18 +0900

    cssファイルを移動

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./tailwind.css";
+import "./assets/tailwind.css";
 import { App } from "./App.jsx";
 
 ReactDOM.render(<App />, document.getElementById("app"));
