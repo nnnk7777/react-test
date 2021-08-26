c6cf9fa96cf9e83c31ace8eaba7ed109ab6eedde
nonaka <electro0701@gmail.com>
2021-08-26 22:27:41 +0900

    Add babel/pollyfill

--- webpack.common.js
+++ webpack.common.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
-    index: path.resolve(__dirname, "./src/index.js"),
+    index: ["@babel/polyfill", path.resolve(__dirname, "./src/index.js")],
   },
   output: {
     path: path.resolve(__dirname, "./dist"),
